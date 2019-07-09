3f4030eb292f90e0ba22df2ffc6aecca4a3511d7
hanna chae <hannacne@naver.com>
2019-07-09 13:00:20 +0900

    switch

---
+++ day03-ControlFlow/src/control/swtchstmt/ReportGrade.java
@@ -0,0 +1,24 @@
+package control.swtchstmt;
+
+/**
+ * 입력된 점수에 대한 학점을 계산
+ * switch 구문을 사용
+ * -------------------------------------------
+ * A : 90점 이상 
+ * B : 80 ~ 89 점 사이 
+ * C : 60 ~ 79 점 사이 
+ * D : 40 ~ 59 점 사이 
+ * F : 이하 나머지
+ * ------------------------------------------- 
+ * printf 로 출력
+ * @author 304
+ *
+ */
+public class ReportGrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
