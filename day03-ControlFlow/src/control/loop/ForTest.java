5f3619aaaa525f16a536f62a82248b3d73a6c4f3
hanna chae <hannacne@naver.com>
2019-07-10 14:43:58 +0900

    for 추가 문제

---
+++ day03-ControlFlow/src/control/loop/ForTest.java
@@ -0,0 +1,25 @@
+package control.loop;
+
+/**
+ * 1. 50 ~ 0 까지 짝수만 역순으로 출력
+ *   (1) continue 없이
+ *   (2) continue 사용
+ * 
+ * --------------------------------------
+ * 2. 1 ~ 10 까지 제곱표를 출력
+ *     1 x  1 =   1
+ *     2 x  2 =   4
+ *    ...
+ *    10 x 10 = 100
+ * --------------------------------------    
+ * @author 304
+ *
+ */
+public class ForTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
