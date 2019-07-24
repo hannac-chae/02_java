13ec880f1eb7a639122359b9a62975def44bfb44
hannac <hannacne@gmail.com>
2019-07-24 10:17:40 +0900

    Product 구현

--- day13-Product/src/product/Product.java
+++ day13-Product/src/product/Product.java
@@ -48,6 +48,113 @@ package product;
  *
  */
 public class Product {
+	
+	// 1. 멤버 변수 선언부
+	/** 제품 코드 */
+	String prodCode;
+	/** 제품 명 */
+	String prodName;
+	/** 제품 가격 */
+	int price;
+	/** 재고 수량 */
+	int quantity;
+	
+	// 2. 생성자 선언부
+	/**
+	 * 기본 생성자
+	 */
+	Product() {
+		
+	}
+	
+	/**
+	 * 제품 코드 필드만 초기화하는 생성자
+	 * @param prodCode
+	 */
+	Product(String prodCode) {
+		this.prodCode = prodCode;
+	}
+	
+	/**
+	 * 모든 필드를 초기화하는 생성자
+	 * @param prodCode
+	 * @param prodName
+	 * @param price
+	 * @param quantity
+	 */
+	Product(String prodCode, String prodName, int price, int quantity) {
+		this(prodCode);
+		this.prodName = prodName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	// 3. 메소드 선언부
+	/**
+	 * 제품 정보를 출력
+	 */
+	public void print() {
+		System.out.printf("제품 정보 [제품코드 : %s, 제품명 : %s"
+				         + ", 가격 %d: , 재고수량 : %d]%n"
+				         , prodCode, prodName, price, quantity);
+	}
+	
+	/**
+	 * 입력된 percentage만큼 할인을 진행할 경우
+	 * 판매 가격이 얼마가 되는지 계산하여 리턴
+	 * @param percentage
+	 * @return
+	 */
+	public int discount(double percentage) {
+		int price = this.price;
+		
+		if (percentage > 0) {
+			price = price - (int)(this.price * (percentage));
+		}
+		
+		return price;
+	}
+
+	/**
+	 * 제품을 출고(판매)하고 재고 수량을 감소시킨다.	 
+	 * @param amount
+	 */
+	public void sell(int amount) {
+		// 재고 수량이 출고하려는 수량보다 크거나 같을때만
+		// 출고함.
+		if (this.quantity >= amount) {
+			this.quantity -= amount;
+		}
+	}
+	
+	/**
+	 * 제품이 입고되어 재고 수량을 증가시킨다.
+	 * @param amount
+	 */
+	public void buy(int amount) {
+		// 입고된 만큼 재고수량 증가 후 저장반영
+		this.quantity += amount;		
+	}
+
+	// Object 클래스에서 나도 모르는 사이 상속받은
+	// toString() 메소드를 재정의 해보자.
+	// 1. 메소드 헤더가 동일 : 리턴타입 메소드이름 (매개변수 목록)
+	@Override
+	public String toString() {
+		String strProduct = String.format(
+				"제품 정보 [제품코드 : %s, 제품명 : %s"
+		                 + ", 가격 %,d: , 재고수량 : %d]%n"
+		         , prodCode, prodName, price, quantity);
+		return strProduct;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
 
